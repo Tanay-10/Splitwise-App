ca8fd777748ccec924e12eb36c47989a0012ff1b
Tanay-10 <agarwaltanay2002@gmail.com>
2022-02-02 23:51:20 +0530

    Textfields, elevated button, navigation push

--- lib/screens/expense.dart
+++ lib/screens/expense.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:split_wise/routing.dart' as routing;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AddExpense extends StatelessWidget {
   const AddExpense({Key? key}) : super(key: key);
@@ -8,11 +10,14 @@ class AddExpense extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: CupertinoColors.activeGreen,
+        ),
         titleSpacing: 0,
         title: Text(
           "Add expense",
           style: TextStyle(
-            color: Colors.black87,
+            color: Colors.black87.withOpacity(0.7),
             fontSize: 22,
           ),
         ),
@@ -28,13 +33,18 @@ class AddExpense extends StatelessWidget {
         ],
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Card(
+            color: Colors.white70,
+            elevation: 5,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(7.0),
             ),
             child: Container(
-              height: 50,
+              decoration: BoxDecoration(border: Border()),
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              height: 60,
               width: double.maxFinite,
               child: Row(
                 children: [
@@ -43,9 +53,215 @@ class AddExpense extends StatelessWidget {
                     style: TextStyle(
                       fontSize: 20,
                     ),
-                  )
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 1 / 4,
+                        height: 40,
+                        child: Card(
+                          child: Center(
+                            child: Text(
+                              "Kunal",
+                              style: TextStyle(
+                                fontSize: 22,
+                                color: Colors.black87.withOpacity(0.8),
+                              ),
+                            ),
+                          ),
+                          elevation: 4,
+                          shape: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              width: 2,
+                              color: CupertinoColors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: -15,
+                        child: Icon(
+                          CupertinoIcons.profile_circled,
+                          color: CupertinoColors.activeGreen,
+                          size: 40,
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
+            ),
+          ),
+          SizedBox(
+            height: 35,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 3 / 4,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 50,
+                  width: 55,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    clipBehavior: Clip.none,
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      elevation: 5,
+                      side: BorderSide(width: 3, color: Colors.white),
+                    ),
+                    child: FaIcon(
+                      FontAwesomeIcons.clipboardList,
+                      color: Colors.black87.withOpacity(0.7),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 2 / 4,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    autofocus: false,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black87,
+                    ),
+                    cursorColor: CupertinoColors.activeGreen,
+                    cursorHeight: 30,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5),
+                        ),
+                        borderSide: BorderSide(
+                          color: CupertinoColors.activeGreen,
+                          width: 2,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5),
+                        ),
+                        borderSide: BorderSide(
+                          color: CupertinoColors.activeGreen,
+                          width: 2,
+                        ),
+                      ),
+                      focusColor: CupertinoColors.activeGreen,
+                      contentPadding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                      isDense: true,
+                      hintText: " Enter a subject",
+                      hintStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black87.withOpacity(0.7),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 3 / 4,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 50,
+                  width: 55,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      elevation: 5,
+                      side: BorderSide(width: 3, color: Colors.white),
+                    ),
+                    child: FaIcon(
+                      FontAwesomeIcons.rupeeSign,
+                      color: Colors.black87.withOpacity(0.7),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 2 / 4,
+                  child: TextField(
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 30,
+                    ),
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
+                    autofocus: false,
+                    cursorColor: CupertinoColors.activeGreen,
+                    cursorHeight: 30,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                      isDense: true,
+                      hintText: " 0.00",
+                      hintStyle: TextStyle(
+                        fontSize: 30,
+                        color: Colors.black87.withOpacity(0.7),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5),
+                        ),
+                        borderSide: BorderSide(
+                          color: CupertinoColors.activeGreen,
+                          width: 2,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5),
+                        ),
+                        borderSide: BorderSide(
+                          color: CupertinoColors.activeGreen,
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 3 / 5,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                elevation: 10,
+              ),
+              child: Text(
+                "Paid by you and split equally",
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              onPressed: () {
+                print("Payment");
+                Navigator.pushNamed(context, routing.expenseSplitId);
+              },
             ),
           )
         ],
