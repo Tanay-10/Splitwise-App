2834b1bf3649b093f3018afb1b1f89ccf33b8358
Tanay-10 <agarwaltanay2002@gmail.com>
2022-02-04 00:40:56 +0530

    Scrolling listview of expenses, refactored activity and account tab

--- lib/navTabs/Friends.dart
+++ lib/navTabs/Friends.dart
@@ -4,6 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../routing.dart' as routing;
 import 'Account.dart';
 import 'Activity.dart';
+import 'package:split_wise/screens/settle_up.dart';
 
 class FriendsTab extends StatefulWidget {
   const FriendsTab({Key? key}) : super(key: key);
@@ -31,31 +32,6 @@ class _FriendsTabState extends State<FriendsTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Colors.white70,
-      //   elevation: 2,
-      //   actions: [
-      //     IconButton(
-      //       splashRadius: 22,
-      //       splashColor: CupertinoColors.systemGrey,
-      //       color: Colors.black54,
-      //       iconSize: 30,
-      //       onPressed: () {},
-      //       icon: Icon(Icons.search),
-      //     ),
-      //     IconButton(
-      //       splashRadius: 22,
-      //       splashColor: CupertinoColors.systemGrey,
-      //       color: Colors.black54,
-      //       iconSize: 30,
-      //       onPressed: () {},
-      //       icon: Icon(Icons.person_add_alt),
-      //     ),
-      //     SizedBox(
-      //       width: 10,
-      //     )
-      //   ],
-      // ),
       body: PageView(
         controller: pageController,
         children: [
@@ -86,7 +62,8 @@ class _FriendsTabState extends State<FriendsTab> {
                 )
               ],
             ),
-            body: Column(
+            body: ListView(
+              // crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
                   child: Text(
@@ -99,323 +76,19 @@ class _FriendsTabState extends State<FriendsTab> {
                   ),
                   padding: EdgeInsets.fromLTRB(10, 15, 0, 8),
                 ),
+                ExpenseTrack(),
+                ExpenseTrack(),
+                ExpenseTrack(),
+                ExpenseTrack(),
+                ExpenseTrack(),
+                ExpenseTrack(),
+                ExpenseTrack(),
+                ExpenseTrack(),
               ],
             ),
           ),
-          Scaffold(
-            floatingActionButton: FloatingAction(),
-          ),
-          Scaffold(
-            appBar: AppBar(
-              elevation: 0.1,
-              backgroundColor: Colors.white70,
-              titleSpacing: 24,
-              title: Text(
-                "Account",
-                style: TextStyle(
-                  color: Colors.black87.withOpacity(0.75),
-                  fontSize: 28,
-                ),
-              ),
-            ),
-            body: Container(
-              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: Scrollbar(
-                thickness: 8,
-                radius: Radius.circular(20),
-                showTrackOnHover: true,
-                isAlwaysShown: true,
-                child: ListView(
-                  // mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            width: 0.7,
-                            color: Colors.grey,
-                          ),
-                          top: BorderSide(
-                            width: 0.7,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                      height: 100,
-                      // padding: EdgeInsets.fromLTRB(7, 10, 7, 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          CircleAvatar(
-                            radius: 30,
-                            // backgroundImage: AssetImage("lib/assets/arg.jpg"),
-                            backgroundColor: Colors.teal.shade500,
-                            child: const Text(
-                              "TA",
-                              style: TextStyle(fontSize: 24),
-                            ),
-                          ),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Tanay Agarwal",
-                                style: TextStyle(
-                                  color: CupertinoColors.black.withOpacity(0.7),
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w700,
-                                  letterSpacing: 0.5,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text(
-                                "getnada1234@zero.com",
-                                style: TextStyle(color: Colors.grey.shade600),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            width: 90,
-                          ),
-                          IconButton(
-                            highlightColor: Colors.grey.withOpacity(0.7),
-                            iconSize: 28,
-                            splashRadius: 22,
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.edit_outlined,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 22,
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: const <Widget>[
-                              Icon(
-                                Icons.qr_code_outlined,
-                                size: 30,
-                              ),
-                              SizedBox(
-                                width: 25,
-                              ),
-                              Text(
-                                "Scan code",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Row(
-                            children: const <Widget>[
-                              Icon(
-                                CupertinoIcons.suit_diamond,
-                                size: 30,
-                              ),
-                              SizedBox(
-                                width: 25,
-                              ),
-                              Text(
-                                "Splitwise Pro",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            "Preferences",
-                            style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 13,
-                          ),
-                          Row(
-                            children: const <Widget>[
-                              Icon(
-                                CupertinoIcons.envelope_fill,
-                                size: 30,
-                              ),
-                              SizedBox(
-                                width: 25,
-                              ),
-                              Text(
-                                "Email Settings",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Row(
-                            children: const <Widget>[
-                              Icon(
-                                CupertinoIcons.bell_solid,
-                                size: 30,
-                              ),
-                              SizedBox(
-                                width: 25,
-                              ),
-                              Text(
-                                "Device and push notification settings",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Row(
-                            children: const <Widget>[
-                              Icon(
-                                Icons.lock_outline_rounded,
-                                size: 30,
-                              ),
-                              SizedBox(
-                                width: 25,
-                              ),
-                              Text(
-                                "Passcode",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            "Feedback",
-                            style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 13,
-                          ),
-                          Row(
-                            children: const <Widget>[
-                              Icon(
-                                Icons.star_purple500_sharp,
-                                size: 30,
-                              ),
-                              SizedBox(
-                                width: 25,
-                              ),
-                              Text(
-                                "Rate Splitwise",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Row(
-                            children: const <Widget>[
-                              Icon(
-                                CupertinoIcons.question_square,
-                                size: 30,
-                              ),
-                              SizedBox(
-                                width: 25,
-                              ),
-                              Text(
-                                "Contact Splitwise support",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(left: 13),
-                      height: 86,
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            width: 0.7,
-                            color: Colors.grey,
-                          ),
-                          top: BorderSide(
-                            width: 0.7,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.logout,
-                            size: 30,
-                            color: CupertinoColors.activeGreen,
-                          ),
-                          SizedBox(
-                            width: 25,
-                          ),
-                          Text(
-                            "Log out",
-                            style: TextStyle(
-                              color: CupertinoColors.activeGreen,
-                              fontSize: 19,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
+          Activity(),
+          Accounts(),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -531,6 +204,60 @@ class _FriendsTabState extends State<FriendsTab> {
   }
 }
 
+class ExpenseTrack extends StatelessWidget {
+  const ExpenseTrack({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        width: double.infinity,
+        height: 70,
+        padding: EdgeInsets.fromLTRB(15, 5, 15, 0),
+        child: Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "ABC",
+              style: TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Spacer(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  "owes you",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w700,
+                    color: CupertinoColors.activeOrange,
+                  ),
+                ),
+                Text(
+                  "\u{20B9}1000.00",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    color: CupertinoColors.activeOrange,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      onTap: () {
+        Navigator.pushNamed(context, routing.settleUpId);
+      },
+    );
+  }
+}
+
 //
 // class FriendsTab extends StatefulWidget {
 //   const FriendsTab({Key? key}) : super(key: key);
@@ -581,43 +308,42 @@ class _FriendsTabState extends State<FriendsTab> {
 //   }
 // }
 
-class ActivityTab extends StatefulWidget {
-  const ActivityTab({Key? key}) : super(key: key);
-
-  @override
-  _ActivityTabState createState() => _ActivityTabState();
-}
-
-class _ActivityTabState extends State<ActivityTab> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingAction(),
-      appBar: AppBar(
-        backwardsCompatibility: true,
-        backgroundColor: CupertinoColors.activeGreen,
-      ),
-      // body: _buildBody(),
-    );
-  }
-}
-
-class AccountTab extends StatefulWidget {
-  const AccountTab({Key? key}) : super(key: key);
-
-  @override
-  _AccountTabState createState() => _AccountTabState();
-}
-
-class _AccountTabState extends State<AccountTab> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      // body: _buildBody(),
-    );
-  }
-}
+// class ActivityTab extends StatefulWidget {
+//   const ActivityTab({Key? key}) : super(key: key);
+//
+//   @override
+//   _ActivityTabState createState() => _ActivityTabState();
+// }
+//
+// class _ActivityTabState extends State<ActivityTab> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       floatingActionButton: FloatingAction(),
+//       appBar: AppBar(
+//         backgroundColor: CupertinoColors.activeGreen,
+//       ),
+//       // body: _buildBody(),
+//     );
+//   }
+// }
+//
+// class AccountTab extends StatefulWidget {
+//   const AccountTab({Key? key}) : super(key: key);
+//
+//   @override
+//   _AccountTabState createState() => _AccountTabState();
+// }
+//
+// class _AccountTabState extends State<AccountTab> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       // body: _buildBody(),
+//     );
+//   }
+// }
 
 class FloatingAction extends StatelessWidget {
   const FloatingAction({Key? key}) : super(key: key);
