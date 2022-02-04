d89c3f527e5e80db3ed1b0afb688d2226a0feabc
Tanay-10 <agarwaltanay2002@gmail.com>
2022-02-05 00:57:56 +0530

    Gesture Detector, description screen route, minor UI changes

--- lib/navTabs/Activity.dart
+++ lib/navTabs/Activity.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:split_wise/navTabs/Friends.dart';
+import 'package:split_wise/routing.dart' as routing;
 
 class Activity extends StatelessWidget {
   const Activity({Key? key}) : super(key: key);
@@ -50,49 +51,54 @@ class ActivityCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 75,
-      padding: EdgeInsets.fromLTRB(10, 7, 10, 0),
-      child: Row(
-        children: [
-          Icon(
-            Icons.sticky_note_2_rounded,
-            color: Colors.black87.withOpacity(0.6),
-            size: 55,
-          ),
-          SizedBox(
-            width: 15,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'You added "18 Dec - 18 Jan".',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.9),
-                  fontSize: 17,
-                  fontWeight: FontWeight.w600,
+    return GestureDetector(
+      child: Container(
+        height: 75,
+        margin: EdgeInsets.fromLTRB(10, 7, 10, 5),
+        child: Row(
+          children: [
+            Icon(
+              Icons.sticky_note_2_rounded,
+              color: Colors.black87.withOpacity(0.6),
+              size: 55,
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'You added "18 Dec - 18 Jan".',
+                  style: TextStyle(
+                    color: Colors.black54.withOpacity(0.84),
+                    fontSize: 17,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-              Text(
-                "You get back \u{20B9}7,972.00",
-                style: TextStyle(
-                  color: CupertinoColors.activeGreen,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w400,
+                Text(
+                  "You get back \u{20B9}2500.00",
+                  style: TextStyle(
+                    color: CupertinoColors.activeGreen,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
-              ),
-              Text(
-                "4 days ago, 7:16 PM",
-                style: TextStyle(
-                  color: CupertinoColors.inactiveGray,
+                Text(
+                  "4 days ago, 7:16 PM",
+                  style: TextStyle(
+                    color: CupertinoColors.inactiveGray,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
+      onTap: () {
+        Navigator.pushNamed(context, routing.activityDescId);
+      },
     );
   }
 }
