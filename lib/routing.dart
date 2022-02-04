e31dc25fb41ba16b3e51493bda98278170d67bb7
Tanay-10 <agarwaltanay2002@gmail.com>
2022-02-05 01:00:14 +0530

    2 new screen routes

--- lib/routing.dart
+++ lib/routing.dart
@@ -4,3 +4,5 @@ const String accountTabId = '/account';
 const String expenseScreenId = '/screens/expense';
 const String expenseSplitId = '/screens/split';
 const String settleUpId = '/screens/settle_up';
+const String recordPaymentId = '/screens/record_payment';
+const String activityDescId = '/screens/activity_desc';
