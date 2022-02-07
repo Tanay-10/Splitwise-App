b93dfd8bdd1644f563fdbc48fba05d7e6b7f50e6
Tanay-10 <agarwaltanay2002@gmail.com>
2022-02-07 23:08:41 +0530

    add contact route created

--- lib/routing.dart
+++ lib/routing.dart
@@ -6,3 +6,4 @@ const String expenseSplitId = '/screens/split';
 const String settleUpId = '/screens/settle_up';
 const String recordPaymentId = '/screens/record_payment';
 const String activityDescId = '/screens/activity_desc';
+const String newContactId = '/screens/new_contact';
