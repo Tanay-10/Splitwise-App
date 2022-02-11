c3188d3ed24ae5e50a459b7ead407364eefd14a0
Tanay-10 <agarwaltanay2002@gmail.com>
2022-02-12 00:09:29 +0530

    minor change

--- lib/routing.dart
+++ lib/routing.dart
@@ -1,7 +1,7 @@
 const String friendsTabId = '/';
 const String activityTabId = '/activity';
 const String accountTabId = '/account';
-const String expenseScreenId = '/screens/expense';
+const String addExpenseScreenId = '/screens/add_expense';
 const String expenseSplitId = '/screens/split';
 const String settleUpId = '/screens/settle_up';
 const String recordPaymentId = '/screens/record_payment';
