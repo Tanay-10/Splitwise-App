0c36a9c9546d110aba1a58a5f2ce3477cee54c41
Tanay-10 <agarwaltanay2002@gmail.com>
2022-02-14 18:20:43 +0530

    class name refactored

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => ExpenseData(),
+      create: (context) => AccountData(),
       child: MaterialApp(
         theme: ThemeData.light(),
         initialRoute: routing.friendsTabId,
